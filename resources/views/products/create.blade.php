3e29a0ab974e8bdfedfedb46aee2fb09b62aedb7
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-04 16:39:52 +0000

    Create add new product page

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create a product</h1>
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
+    <form method="post" action="{{route('product.store')}}">
+        @csrf
+        @method('post')
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name">
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="Qty">
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="Price">
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="Description">
+        </div>
+        <div>
+            <input type="submit" value="Save a new product">
+        </div>
+    </form>
+</body>
+</html>
